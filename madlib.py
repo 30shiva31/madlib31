145f77dd9f0b7b3a5ddbb3830be6662b9ab44cc6
30shiva31 <shiva3one@gmail.com>
2021-11-04 13:29:06 +0530

    first commit

---
+++ madlib.py
@@ -0,0 +1,24 @@
+print("Hii, Welcome to madlib game")
+print("\nHow to play \n\nEnter the appropriate words as the prompts guide you to and you get to read a small story in the end ;)")
+print("\n\ngame starts now.......\n\n")
+yourname = input("Your name: ")
+age = input("Your age: ")
+socialmedia = input("Name of the social platform you spend more time: ")
+planet1 = input("Enter a planet name other than earth: ")
+planet2 = input("Enter an another planet name: ")
+organhole1 = input(
+    "Enter an outer body part which has hole to it: (Try not to text nose) ")
+organhole2 = input("Enter an another outer body part which has hole to it: ")
+fluid1 = input("Name of your favourite drink: ")
+game1 = input("Name of my favourite game: ")
+friendname = input("Name of your bestfriend: ")
+country = input("Name of a country where you do not want to go: ")
+ambition = input("(dream job or ambition)You want to become a: ")
+pick_best_worst = input(" Pick an option 1)best 2)worst 3)good 4)bad: ")
+dreamcountry = input("Name of your favourite dream country: ")
+scenery = input(
+    "Favourite scenery( example: sunset, sunrise, foggy mountains, beach....etc): ")
+
+print("\n\nSTORY TIME..............\n\n\n")
+madlib = f"Hey I am {yourname}. I am {age} years old and I have no idea of drinking water properly. I learn't to drink water by watching in {socialmedia}. Before I used to drink water from my nose lol. I also recently discovered that the life on the planet {planet1} is not same as the life on the planet {planet2} because people living on {planet1} breath from {organhole1} and eat from {organhole2} where as people living on {planet2} do not breathe at all, they survive by drinking {fluid1} and breathing for them is not as important as it is for us. I also play {game1} and I always win against my friend {friendname}, looks like am a pro in this game, am going to represent {country} in upcoming olympics. My ambition is to become a {ambition}, and I am very {pick_best_worst} at working my targets everyday so that I can reach to my dream place some day, also I want to settle in {dreamcountry} because I love to enjoy {scenery} every day."
+print(madlib)
